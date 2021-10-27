30ebdb0c260485796cf97c57e7ec90a834f3b1aa
Ken Human <ken@kenhuman.com>
2021-10-27 10:58:16 -0400

    update mongodb init

--- src/client.ts
+++ src/client.ts
@@ -8,40 +8,45 @@ process.on('unhandledRejection', (error) => {
 	console.error('Unhandled promise rejection:', error);
 });
 
-const client = new Client({
-    prefix: "!",
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-    ],
-    classes: [
-        path.join(__dirname, "commands", "**/*.{ts,js}")
-    ],
-    botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
-    silent: true,
-});
-
-client.once("ready", async () => {
-    await client.initApplicationCommands({
-        guild: { log: true },
-        global: { log: true },
-    });
-    await client.initApplicationPermissions();
+(async () => {
 
     await mongo.initialize();
     await spellDb.initialize();
 
-    console.log("Bot started");
-});
+    const client = new Client({
+        prefix: "!",
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.GUILD_VOICE_STATES,
+        ],
+        classes: [
+            path.join(__dirname, "commands", "**/*.{ts,js}")
+        ],
+        botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
+        silent: true,
+    });
+    
+    client.once("ready", async () => {
+        await client.initApplicationCommands({
+            guild: { log: true },
+            global: { log: true },
+        });
+        await client.initApplicationPermissions();
+    
+        console.log("Bot started");
+    });
+    
+    client.on("interactionCreate", (interaction: Interaction) => {
+        client.executeInteraction(interaction);
+    });
+    
+    client.on("messageCreate", (message: Message) => {
+        client.executeCommand(message);
+    });
+    
+    client.login(process.env.BOT_TOKEN ?? "");
 
-client.on("interactionCreate", (interaction: Interaction) => {
-    client.executeInteraction(interaction);
-});
+})();
 
-client.on("messageCreate", (message: Message) => {
-    client.executeCommand(message);
-});
-
-client.login(process.env.BOT_TOKEN ?? "");
